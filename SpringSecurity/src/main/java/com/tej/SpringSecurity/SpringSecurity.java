4ddc69c411b7bbb810295b07c4a0e427ef3ce639
Tejprakash18 <tej22upa.dhyay@gmail.com>
2025-04-17 22:36:25 +0530

    Spring security

---
+++ SpringSecurity/src/main/java/com/tej/SpringSecurity/SpringSecurity.java
@@ -0,0 +1,13 @@
+package com.tej.SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity.class, args);
+	}
+
+}
